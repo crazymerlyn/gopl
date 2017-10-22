f6cd34d6f135bcc7d297a77253e4c27aef68710e
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-22 12:44:32 +0530

    Solved exercise 1.9 - Print status code

--- fetch/main.go
+++ fetch/main.go
@@ -18,6 +18,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
+		fmt.Printf("Status Code: %v\n", resp.Status)
 		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
